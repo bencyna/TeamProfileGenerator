f6fa8b71cb594b6c7d9cb5e307bd8111db9c60db
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2021-02-16 18:29:23 +1030

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const Employee = require ("./lib/Employee");
+// const Engineer = require ("./lib/engineer");
+// const Intern = require ("./lib/intern");
+const Manager = require ("./lib/Manager");
+
+const defineManager = new Manager (officeNumber);
+
+defineManager.office();
+
+// all inquirer here
